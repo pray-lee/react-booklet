62882bcfcd7eb59177dc1c68fed44b8740694c10
pray-lee <praycis@gmail.com>
2020-08-19 17:07:18 +0800

    update

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -8,7 +8,19 @@ const appState = {
         color: 'blue'
     }
 }
+// 定义一个dispatch方法，来负责数据的修改
+// 所有对数据的操作必须通过dispatch函数。它接收一个参数action, 这个action是一个普通的javascript对象，里面一个type字段标明你想干什么,dispatch 在 swtich 里面会识别这个 type 字段，能够识别出来的操作才会执行对 appState 的修改
+function dispatch(action) {
+    switch(action.type) {
+        case 'UPDATE_TITLE_TEXT':
+            appState.title.text = action.text
+            break
+        case 'UPDATE_TITLE_COLOR':
+            appState.title.color = action.color
+    }
+}
 
+// other operation
 function renderApp(appState) {
     renderTitle(appState.title)
     renderContent(appState.content)
@@ -27,3 +39,4 @@ function renderContent(content) {
 }
 
 renderApp(appState)
+
