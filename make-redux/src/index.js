40269c9025b2cc5ab51488fed9dba2832295c2ce
pray-lee <praycis@gmail.com>
2020-08-19 16:55:22 +0800

    update

---
+++ make-redux/src/index.js
@@ -0,0 +1,29 @@
+const appState = {
+    title: {
+        text: 'React.js',
+        color: 'red'
+    },
+    content: {
+        text: '内容',
+        color: 'blue'
+    }
+}
+
+function renderApp(appState) {
+    renderTitle(appState.title)
+    renderContent(appState.content)
+}
+
+function renderTitle(title) {
+    const titleDOM = document.getElementById('title')
+    titleDOM.innerHTML = title.text
+    titleDOM.style.color = title.color
+}
+
+function renderContent(content) {
+    const contentDOM = document.getElementById('content')
+    contentDOM.innerHTML =  content.text
+    contentDOM.style.color = content.color
+}
+
+renderApp(appState)
